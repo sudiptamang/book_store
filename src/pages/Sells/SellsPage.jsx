afd713464b9b37721c7f866197919851cc07cfa1
Pemba lama <pembatshringgoel@gmail.com>
2022-08-24 19:23:27 +0545

    Update sell page

--- src/pages/Sells/SellsPage.jsx
+++ src/pages/Sells/SellsPage.jsx
@@ -1,40 +1,46 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
-import Button from "../../components/Button/Button";
-import "./SellsPage.css"
+
+import "./SellsPage.css";
 
 const SellsPage = () => {
-  return <div>
-    <NavBar/>
-    <div className="SellsContainers">
-      <h1 className="SellsPageWelCome">Welcome to the <span className="WelCome">Book Store</span>  family!</h1>
-      <p className="SellInfo">Once you and the our book store come to an agreement, you should discuss the transition with the rest of your account.</p>
-     <div className="SellsContainer">
-      <h2 className="ImportImg">Upload Your Book's Image</h2>
-        <input type="file" id="file-input" width="40px" />
-        <div className="sellsInputs">
+  return (
+    <div>
+      <NavBar />
+      <div className="SellsContainers">
+        <h1 className="SellsPageWelCome">
+          Welcome to the <span className="WelCome">Book Store</span> family!
+        </h1>
+        <p className="SellInfo">
+          Once you and the our book store come to an agreement, you should
+          discuss the transition with the rest of your account.
+        </p>
+        <div className="SellsContainer">
+          <h2 className="ImportImg">Upload Your Book's Image</h2>
+          <input type="file" id="file-input" width="40px" />
 
-      <div >
-        <div className="BookName">Name </div><input type="text" className=" BookInfo" placeholder=" Book's Name"/>
-      </div>      
-      <div>
-      <div className="BookName">Title </div>  <input type="text" className=" BookInfo" placeholder=" Book's Title"/>
-      </div>     
- 
-       <div>
-       <div className="BookName"> Authors </div> <input type="text" className=" BookInfo" placeholder=" Author's Name"/>
-        </div> 
-            
-        <div>
-      <div className="BookName">Price </div> <input type="number" className=" BookInfo" placeholder="Price"/>
-      </div>  
-       <div>
-      <div className="BookName">Quantity </div> <input type="number" className=" BookInfo" placeholder="Quantity"/>
-      </div> 
+          <div className="BookName">Name </div>
+          <input type="text" className=" BookInfo" placeholder=" Book's Name" />
+          <div className="BookName">Title </div>
+          <input
+            type="text"
+            className=" BookInfo"
+            placeholder=" Book's Title"
+          />
+          <div className="BookName"> Authors </div>
+          <input
+            type="text"
+            className=" BookInfo"
+            placeholder=" Author's Name"
+          />
+          <div className="BookName">Price </div>
+          <input type="number" className=" BookInfo" placeholder="Price" />
+          <div className="BookName">Quantity </div>
+          <input type="number" className=" BookInfo" placeholder="Quantity" />
+          <button className="SellsBtn">Sell</button>
+        </div>
       </div>
-        <div className="sellButton"><Button buttonLabel={ "Sell"} /></div>
-     </div>
     </div>
-  </div>;
+  );
 };
 export default SellsPage;
