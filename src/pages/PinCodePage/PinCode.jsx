bfc7cb763a8a0a6c028eef49f04d8def4353a4e6
Pemba lama <pembatshringgoel@gmail.com>
2022-09-01 19:35:04 +0545

    Updated in pinCode

--- src/pages/PinCodePage/PinCode.jsx
+++ src/pages/PinCodePage/PinCode.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+// import React from "react";
 import BackBar from "../../components/BackBar/BackBar";
 import "./PinCode.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import photo from "../../Assets/book.webp";
+
 const PinCode = () => {
+  const [reqPinCode, setReqPinCode] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handlePinCode = (event) => {
+    console.log("happy");
+    event.preventDefault();
+    fetch("http://localhost:3030/user/code/verify", {
+      method: "POST",
+      body: JSON.stringify({}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          navigate("/ForgotPassword");
+          console.log("success", data.success);
+        } else if (PinCode == "" || PinCode == undefined) {
+          setReqPinCode("Enter a code", data.reqPinCode);
+        } else {
+          setMessage(data.message);
+          console.log(data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <BackBar />
@@ -15,12 +47,21 @@ const PinCode = () => {
             email.
           </p>
           <div>
-            <input type="text" className="pincode" placeholder="code" />
+            <input
+              type="text"
+              className="pincode"
+              placeholder="code"
+              onChange={(e) => setReqPinCode(e.target.value)}
+            />
           </div>
-          {/* 
-          <Link to={"/Forgotpassword"}> */}
-          <button className="PinCodeContinue">Continue</button>
-          {/* </Link> */}
+          <p className="message">
+            {reqPinCode}
+            {message}
+          </p>
+
+          <button onClick={handlePinCode} className="PinCodeContinue">
+            Continue
+          </button>
         </div>
         <div className="PinCodeHamroKitab">
           <div className="Ernest">
